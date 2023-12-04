3d036572233ec615aaaaa8815ee808f094f563ff
lacinajir <70260403+lacinajir@users.noreply.github.com>
2023-12-04 16:20:07 +0100

    Update script.js
    Sorting

--- script.js
+++ script.js
@@ -72,29 +72,33 @@ function sortTable(n) {
   table = document.getElementById("results");
   switching = true;
   dir = "asc";
+  
   while (switching) {
     switching = false;
     rows = table.rows;
+    
     for (i = 1; i < (rows.length - 1); i++) {
       shouldSwitch = false;
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
+      x = parseFloat(rows[i].getElementsByTagName("TD")[n].innerText);
+      y = parseFloat(rows[i + 1].getElementsByTagName("TD")[n].innerText);
+      
       if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        if (x > y) {
           shouldSwitch = true;
           break;
         }
       } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+        if (x < y) {
           shouldSwitch = true;
           break;
         }
       }
     }
+    
     if (shouldSwitch) {
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
-      switchcount ++;
+      switchcount++;
     } else {
       if (switchcount == 0 && dir == "asc") {
         dir = "desc";
